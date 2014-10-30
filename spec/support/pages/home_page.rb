6246ab2d9ed6a3f3317a5fce1feabf1aae06da14
Brandon Smith <brsmith@thoughtworks.com>
2014-10-30 19:15:42 +0000

    Update Home page object with methods

--- spec/support/pages/home_page.rb
+++ spec/support/pages/home_page.rb
@@ -10,4 +10,19 @@ class HomePage < SitePrism::Page
   element :coach_reviews_heading, "h1", text: "Upcoming Reviews"
   element :show_review_link, ".fa-eye"
 
+  def show_review
+    show_review_link.click
+  end
+
+  def create_new_review
+    new_review_link.click
+  end
+
+  def ask_for_feedback
+    ask_feedback_link.click
+  end
+
+  def submit_feedback
+    submit_feedback_link.click
+  end
 end
