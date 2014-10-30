430d3346355ec0d230e5cca04f34cf5738ea2cc8
Brandon Smith <brsmith@thoughtworks.com>
2014-10-30 16:19:59 +0000

    Update Feedback page object to include general comments header

--- spec/support/pages/feedback_page.rb
+++ spec/support/pages/feedback_page.rb
@@ -1,6 +1,5 @@
-class PasswordResetPage < SitePrism::Page
+class FeedbackPage < SitePrism::Page
   set_url '/feedback'
-  
 
   element :full_title, "title"
   element :submit_final_button, "input#submit-final-button"
@@ -8,6 +7,7 @@ class PasswordResetPage < SitePrism::Page
   element :comments_header_link, "h3", text: "Comments"
   element :flash_message, ".flash"
   element :error_message, ".flash-alert",
+    text: "You are not authorized to access this page."
   element :edit_link, "a", text: "Edit"
-    text:"You are not authorized to access this page.")
-  section :okta_form, OktaSection, "section#okta-input"
+  element :general_comments_section, "#ui-accordion-accordion-header-9"
+end
